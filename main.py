0ad837c2961057db8d28f5a555e65985b01a3c21
danielhammerl <daniel.hammerl99@gmx.de>
2023-10-21 09:56:32 +0200

    Init

---
+++ main.py
@@ -0,0 +1,41 @@
+import cv2
+import numpy as np
+from flask import Flask, Response
+from datetime import datetime
+
+app = Flask(__name__)
+
+def get_current_timestamp():
+    now = datetime.now()
+    timestamp = now.strftime("%d.%m.%Y %H:%M:%S")
+    return timestamp
+
+# Replace 'rtsp://your_stream_url' with the RTSP stream URL you want to capture.
+rtsp_url = 'rtsp://localhost:8554/stream'
+cap = cv2.VideoCapture(rtsp_url)
+
+def generate_frames():
+    while True:
+        success, frame = cap.read()
+        if not success:
+            break
+
+        timestamp = get_current_timestamp()
+
+        # Overlay the timestamp on the frame
+        cv2.putText(frame, timestamp, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+
+        ret, buffer = cv2.imencode('.jpg', frame)
+        if not ret:
+            continue
+        frame = buffer.tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/')
+def video_feed():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    from waitress import serve
+    serve(app, host='0.0.0.0', port=8080)
