de46247df5ca514544e28cf866f8cad816b2121a
danielhammerl <daniel.hammerl99@gmx.de>
2023-10-21 10:46:36 +0200

    Init

--- stream-rtsp.sh
+++ stream-rtsp.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-libcamera-vid -t 0 --width=1920 --height=1080 --framerate 24 --inline -o - | cvlc stream:///dev/stdin --sout '#rtp{sdp=rtsp://:8554/stream}' :demux=h264
+libcamera-vid -t 0 --mode=1920:1080:10 --framerate 24 --inline -o - | cvlc stream:///dev/stdin --sout '#rtp{sdp=rtsp://:8554/stream}' :demux=h264
